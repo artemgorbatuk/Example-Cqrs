0b1a5e63b30da2ce1ee86227d73bf8c5e6267eee
Artem Gorbatuk <gorbatuk.am@gmail.com>
2023-01-14 17:42:26 +0100

    create core project. add domain person

---
+++ Core/Domain/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Domain;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string? LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public DateTime? Birth { get; set; }
+    public DateTime? Death { get; set; }
+}
